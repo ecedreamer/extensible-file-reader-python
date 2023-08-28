e525e758f6802de7e6629304d06eb3e40da999c8
Nishma <nishma.kafle225@gmail.com>
2023-08-28 20:59:14 +0545

    process file using multiprocessing

--- src/main.py
+++ src/main.py
@@ -3,36 +3,54 @@ from pprint import pprint
 
 from libs.file_processor import FileProcessor, get_processor_engine
 from utils.logger import logging
+import multiprocessing
+import time
+
 
 logger = logging.getLogger(__name__)
 
 
-def start() -> None:
+def process_file(file_path):
     all_engines = get_processor_engine()
-    print("Available Engines\n", {f"{name}_{engine['version']}" for name, engine in all_engines.items()})
-    while True:
-        file_name = input("Enter file name: ")
-        file_path = f"files/{file_name}"
-        if not Path(file_path).exists():
-            logger.error("File not found in files/ directory.")
-            continue
+    file_extension = file_path.suffix[1:]  # Get file extension without the dot
+    possible_reader = f"{file_extension}_reader"
+    processor_engine_names = list(all_engines.keys())
+    if possible_reader in processor_engine_names:
+        processor_engine_name = possible_reader
+    else:
+        return f"Invalid File: {file_path}"
 
-        processor_engine_name = input("Processor engine: ")
-        if not all_engines.get(processor_engine_name):
-            logger.error(f"Processor engine: {processor_engine_name} is not supported")
-            continue
-        
-        
-        selected_engine = all_engines.get(processor_engine_name)
-        engine_obj = selected_engine.get("reader").create_engine()
-        processor = FileProcessor(engine_obj, file_path)
-        processor.read()
+    if processor_engine_name not in all_engines:
+        return f"Processor engine '{processor_engine_name}' is not supported for file: {file_path}"
+
+    selected_engine = all_engines[processor_engine_name]
+    engine_obj = selected_engine["reader"].create_engine()
+    processor = FileProcessor(engine_obj, file_path)
+    processor.read()
+
+def start():
+    
+    directory_path = "files"
+    if not Path(directory_path).exists():
+        logger.error("File not found in files/ directory.")
+
+    directory = Path(directory_path)
+    file_paths = [file for file in directory.iterdir() if file.is_file()]
+
+    pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())  # Use all available CPU cores
+
+    results = pool.starmap(process_file, [(file_path, ) for file_path in file_paths])
+
+    for result in results:
+        if result is not None:
+            print(result)
 
 
 def main() -> None:
     logging.info("Starting main...")
     try:
         start()
+
     except KeyboardInterrupt:
         logger.warning("Keyboard Interrupt by the User!!")
 
